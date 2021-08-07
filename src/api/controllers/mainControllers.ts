7fe93828d12298c04f6874873f301bfe78f12032
auleki <kodagiwa@gmail.com>
2021-08-07 13:16:15 +0100

    added and defined roomjoin controller

--- src/api/controllers/mainControllers.ts
+++ src/api/controllers/mainControllers.ts
@@ -7,5 +7,10 @@ export class MainController {
   @OnConnect() 
   public onConnection(@ConnectedSocket() socket: Socket, @SocketIO() io: Server) {
     console.log("New Socket Connected", socket.id)
+
+    /* Listening for custom event  */  
+    socket.on("custom_event", (data) => {
+      console.log("Data", data)
+    })
   }
 }
